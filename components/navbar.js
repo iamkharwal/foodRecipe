9b932cfdb7c5198eaff8445620100f085744982a
Atul Kharwal <93377463+iamkharwal@users.noreply.github.com>
2021-12-03 00:33:36 +0530

    Add files via upload

---
+++ components/navbar.js
@@ -0,0 +1,23 @@
+let navbar = () => {
+    return `
+    <nav>
+    <div class="wrap">
+        <div class="logo">
+          <a href="index.html"><h1 style="color:red">FOOD RECIPE</h1></a>  
+        </div>
+        <div class="search">
+        <input type="text" id="search" oninput="debounce(searchItem,1000)" placeholder="Search Meal">
+        </div>
+        <div class="results" id="results"></div>
+        <div class="menu">
+            <ul>
+                <li><a href="index.html">Latest Recipe</a></li>
+                <li><a href="random.html">Recipe of the Day</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+`;
+}
+
+export { navbar }
